5af41f0d5bb5350c7a4ee0095759eb0fc60c4363
Arturas <mbarturas@yahoo.com>
2022-12-14 23:06:51 +0200

    category drop down added

--- server.js
+++ server.js
@@ -35,13 +35,14 @@ app.use(express.json());
 app.use(logger("dev"));
 
 //Use forms for put / delete
-app.use(methodOverride(function (req, res) {
+app.use(methodOverride("_method"));
+/* app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === "object" && "_method" in req.body) {
       let method = req.body._method
       delete req.body._method
       return method
     }
-}));
+})); */
 
 // Setup Sessions - stored in MongoDB
 app.use(
